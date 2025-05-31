0241de1f4c596f83d908dce911eb798ac3a63533
Klaiven <klaivencastro@gmail.com>
2025-05-31 13:59:45 -0300

    Refactor: criada responsividade nos documentos de brieffing

--- src/pages/Briefing/ListaBriefings.tsx
+++ src/pages/Briefing/ListaBriefings.tsx
@@ -17,14 +17,15 @@ const Briefings: React.FC = () => {
             <div className="container-lista-briefings">
                 <ProfileHeader profilePic={imagens.arquiteto} tipoUsuario={"profissional"} />
                 <div className="topo-lista-briefings">
-                    <div className="topo-lista-briefings-col1">
-                        <div><h1>Nossos Briefings</h1>
-                            <p>Escolha e preencha o briefing ideal para seu projeto.</p>
-                        </div>
+                <div className="topo-lista-briefings-col1">
+                    <div>
+                        <h1>Nossos Briefings</h1>
+                        <p>Escolha e preencha o briefing ideal para seu projeto.</p>
                     </div>
-                    <div className="topo-lista-briefings-col2">
-                        <button className="briefing-personalizado">+ Briefing personalizado</button>
-                    </div>
+                </div>
+                <div className="topo-lista-briefings-col2">
+                    <button className="briefing-personalizado">+ Briefing personalizado</button>
+                </div>
                 </div>
                 <FilterBriefing onFilterChange={setFilteredBriefings} />
                 <BriefingList briefings={filteredBriefings} />
